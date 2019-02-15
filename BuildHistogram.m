d6e2ef0b09240124879a9fa0065d3646cf46be4c
Myat Mo <myatmo@outlook.com>
2019-02-15 02:45:36 -0600

    Build the histogram of oriented gradients for each block cell

--- BuildHistogram.m
+++ BuildHistogram.m
@@ -6,32 +6,40 @@ function ori_histo = BuildHistogram(grad_mag, grad_angle, cell_size)
 %n/cell_size where b·c is the round-off operation as shown in Figure 4(a).
 %Description: Given the magnitude and orientation of the gradients per pixel, you can
 %build the histogram of oriented gradients for each cell.
-m = size(grad_mag,1);
+m = size(grad_angle,1);
 n = size(grad_angle,2);
-M = m/cell_size;
-N = n/cell_size;
-nbins = 6;
-
-ori_hisito = zeros(M,N,nbins);
-
-im_filtered = zeros(size(im));
-center_k = floor(size(filter,1)/2)+1;
-
-center_l = floor(size(filter,2)/2)+1;
-for i = 1 : size(im,1)
-    for j = 1 : size(im,2)
-    % filtering
-        v = 0;
-        for k = 1 : size(filter,1)
-            for l = 1 : size(filter,2)
-                i1 = i + k - center_k;
-                j1 = j + l - center_l;
-                if i1 <= 0 || i1 > size(im_filtered,1) || j1 <= 0 || j1 > size(im_filtered,2)
-                    continue;
+M = floor(m/cell_size);
+N = floor(n/cell_size);
+theta = [15 45 75 105 135 165];
+nbins = size(theta,2);
+%ori_histo = zeros(M,N,nbins);
+ori_histo = zeros(M,N,nbins);
+% change the gradient angle value from rad to angle
+grad_angle = radtodeg(grad_angle);
+% Divide the image into cell_size blocks
+for i = 0 : M-1
+    for j = 0 : N-1
+        u = (i*cell_size)+1;
+        v = (j*cell_size)+1;
+        %A(u:u+cell_size-1,v:v+cell_size-1)
+        for k = u:u+cell_size-1
+            for l = v:v+cell_size-1
+                % check the angles
+                if (grad_angle(k,l) >= theta(1)) && (grad_angle(k,l) < theta(2)) % if the angle is [15,45)
+                    ori_histo(i+1,j+1,2) = ori_histo(i+1,j+1,2) + grad_mag(k,l);
+                elseif (grad_angle(k,l) >= theta(2)) && (grad_angle(k,l) < theta(3)) % if the angle is [45,75)
+                    ori_histo(i+1,j+1,3) = ori_histo(i+1,j+1,3) + grad_mag(k,l);
+                elseif (grad_angle(k,l) >= theta(3)) && (grad_angle(k,l) < theta(4)) % if the angle is [75,105)
+                    ori_histo(i+1,j+1,4) = ori_histo(i+1,j+1,4) + grad_mag(k,l);
+                elseif (grad_angle(k,l) >= theta(4)) && (grad_angle(k,l) < theta(5)) % if the angle is [105,135)
+                    ori_histo(i+1,j+1,5) = ori_histo(i+1,j+1,5) + grad_mag(k,l);
+                elseif (grad_angle(k,l) >= theta(5)) && (grad_angle(k,l) < theta(6)) % if the angle is [135,165)
+                    ori_histo(i+1,j+1,6) = ori_histo(i+1,j+1,6) + grad_mag(k,l);
+                else % if the angle is [165,180) U [0,15)
+                    ori_histo(i+1,j+1,1) = ori_histo(i+1,j+1,1) + grad_mag(k,l);
                 end
-                v = v + im(i1,j1)*filter(k,l);
             end
         end
-        im_filtered(i,j) = v;
     end
+    
 end
